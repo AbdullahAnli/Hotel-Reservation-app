97fabde68ca45eab39ceeabeaa8660036d7b1ec8
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-02 14:36:03 +0300

    hotel Service  have been created

--- src/main/java/com/HotelReservationapp/service/HotelService.java
+++ src/main/java/com/HotelReservationapp/service/HotelService.java
@@ -1,4 +1,35 @@
 package com.HotelReservationapp.service;
 
+import com.HotelReservationapp.dao.HotelRepository;
+import com.HotelReservationapp.entity.Hotel;
+import jakarta.servlet.http.PushBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration;
+import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class HotelService {
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    public List<Hotel>getAllHoteells(){
+        return hotelRepository.findAll();
+    }
+    public Optional<Hotel>getHotelById(Long id){
+        return hotelRepository.findById(id);
+    }
+    public Optional<Hotel>getHotelByCity(String city) {
+       return hotelRepository.getHotelByCity(city);
+    }
+    public void SaveOrUpdateHotel(Hotel hotel){
+        hotelRepository.save(hotel);
+    }
+    public void DeleteHotel(Long id){
+        hotelRepository.deleteById(id);
+    }
+
 }
