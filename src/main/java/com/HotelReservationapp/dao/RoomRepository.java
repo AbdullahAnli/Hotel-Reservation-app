c0fd3388247a1480506b7b9bcd7e6428a179f402
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-02 15:17:05 +0300

    Room Repository  have been updates

--- src/main/java/com/HotelReservationapp/dao/RoomRepository.java
+++ src/main/java/com/HotelReservationapp/dao/RoomRepository.java
@@ -3,5 +3,9 @@ package com.HotelReservationapp.dao;
 import com.HotelReservationapp.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    Optional<Room> getRoomByRoomNumber(String roomNumber);
 }
