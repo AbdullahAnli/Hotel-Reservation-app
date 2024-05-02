b93ffa782551fd916a98e5e5a429fe5db4482944
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-02 15:06:12 +0300

    Hotel Repository  have been updates

--- src/main/java/com/HotelReservationapp/dao/HotelRepository.java
+++ src/main/java/com/HotelReservationapp/dao/HotelRepository.java
@@ -3,5 +3,8 @@ package com.HotelReservationapp.dao;
 import com.HotelReservationapp.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HohelRepository extends JpaRepository<Hotel,Long> {
+import java.util.Optional;
+
+public interface HotelRepository extends JpaRepository<Hotel,Long> {
+    Optional<Hotel>getHotelByCity(String city);
 }
